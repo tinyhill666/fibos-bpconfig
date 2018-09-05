d9ea9bb600d33c598632429f193dc9ef29c93800
qiushaoxi <qiushaoxi@163.com>
2018-09-05 13:36:17 +0800

    fix

--- run.js
+++ run.js
@@ -1,6 +1,34 @@
-var fibos = require('fibos');
-var config = require('./config.json')
+var config = {
+    "p2p_peer_address": [
+        "se-p2p.fibos.io:9870",
+        "sl-p2p.fibos.io:9870",
+        "to-p2p.fibos.io:9870",
+        "ca-p2p.fibos.io:9870",
+        "ln-p2p.fibos.io:9870",
+        "va-p2p.fibos.io:9870",
+        "seed.bitze.site:9870",
+        "p2p-mainnet.fibos123.com:9977",
+        "p2p-mainnet.fibos123.com:9977",
+        "seed.fibos.rocks:10100",
+        "seed-mainnet.fibscan.io:9103",
+        "p2p.mainnet.fibos.me:80",
+        "p2p.foshenzhenbp.com:9877",
+        "p2p.eoschina.me:10300",
+        "fibos-p2p.slowmist.io:9870",
+        "fibos.qubitfund.com:9870"
+    ],
+    "config_dir": "./config",
+    "data_dir": "./data",
+    "producer-name": "",
+    "public-key": "",
+    "private-key": "",
+    "parameter": {
+        "genesis-json": "./genesis.json",
+        "delete-all-blocks": true
+    }
+}
 
+var fibos = require('fibos');
 fibos.config_dir = config.config_dir;
 fibos.data_dir = config.data_dir;
 
@@ -21,7 +49,7 @@ fibos.load("producer", {
 	'private-key': JSON.stringify([config["public-key"], config["private-key"]])
 });
 
-fibos.load("chain",config.parameter);
+fibos.load("chain", config.parameter);
 fibos.load("chain_api");
 fibos.enableJSContract = false;
 
