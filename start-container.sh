86ae6aa5b84894dcc134adfb95467f135003de4e
qiushaoxi <qiushaoxi@163.com>
2018-09-05 12:34:04 +0800

    fix

--- start-container.sh
+++ start-container.sh
@@ -4,6 +4,6 @@ docker stop $container_name -t 100
 docker rm -f $container_name
 
 docker run --name $container_name \
-    -v $pwd:/fibos \
+    -v $PWD:/fibos \
     -p $http_port:8888 -p $p2p_port:9876 \
     $docker_tag fibos run.js
